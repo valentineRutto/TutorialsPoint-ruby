afd7191931574732888c31cd4f8f9ee8ab1ebbad
valentinerutto <rutto28@gmail.com>
2015-08-17 12:57:39 +0300

    yield with no parameters

--- blocks.rb
+++ blocks.rb
@@ -6,3 +6,18 @@ block_name{
    ..........
 }
 block can be invoked with the yield statement
+===yield statement===
+    eg:
+def test
+    yield
+    puts "2"
+    yield
+    puts "3"
+
+end
+test{puts "i am number"}
+output:
+i am number
+2
+i am number
+3
