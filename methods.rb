3bda1aae5cc62b5cabe1d2896aaca75b8d91c031
valentinerutto <rutto28@gmail.com>
2015-08-17 12:13:11 +0300

    class methods

--- methods.rb
+++ methods.rb
@@ -2,3 +2,6 @@ methods syntax:
 def sample (*test)
 end
 this takes undefined number of parameters
+====class methods===
+        private=>when a method is defined outsige a classs
+    public=> when a method is defined inside a class
