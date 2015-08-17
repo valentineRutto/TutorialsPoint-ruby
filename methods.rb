c08bef3cf66c835ccf98123c5072ceb6a836c0c3
valentinerutto <rutto28@gmail.com>
2015-08-17 12:25:28 +0300

    declaring and accesing class methods

--- methods.rb
+++ methods.rb
@@ -4,4 +4,16 @@ end
 this takes undefined number of parameters
 ====class methods===
         private=>when a method is defined outsige a classs
-    public=> when a method is defined inside a class
+    public=> when a method is defined inside a class=
+            ===accesing,declaring classmethods===
+            accesing method from a class frst u instantiate the class
+class Accounts
+   def reading_charge
+   end
+   def Accounts.return_date
+   end
+end
+                to acces this class method return_date, you neednotcreate ojects
+                    you just write 
+                    Accounts.return_date
+        
