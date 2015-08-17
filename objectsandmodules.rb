3f474f33732e925bf608dfc451428ede7424945f
valentinerutto <rutto28@gmail.com>
2015-08-17 19:22:14 +0300

    created class and instantiated it

--- objectsandmodules.rb
+++ objectsandmodules.rb
@@ -12,3 +12,13 @@ Global -> available across the entire application
         $var
 constance -> all uppercase, available through the entire application and can only be modified in the scope described.
             eg: VAR
+======CLASS======
+class MyClass
+    def initialize //whatever we want to run immediately the class is run or instantiated
+        @database=connect_to_database
+    def hello
+        puts "hello"
+    end
+end
+        Var=MyClasss.new //instantiate the class
+        var.hello
