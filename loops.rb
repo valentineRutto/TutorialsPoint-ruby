0abc55e9736317d85839f0b8e79c9b5ddfe624d6
valentinerutto <rutto28@gmail.com>
2015-08-17 09:46:35 +0300

    while loop modifier

--- loops.rb
+++ loops.rb
@@ -4,3 +4,22 @@
    code
 end
 the code is separated from the conditions with a do \ or ;
+    ==while modifier==
+            syntax:
+code while condition
+
+OR
+
+begin 
+  code 
+end while conditional 
+    // this executes code while conditional is true
+        
+        If a while modifier follows a begin statement with no rescue or ensure clauses, code is executed once before conditional is evaluated.
+                eg:
+            $i=0
+            $num=5
+            begin
+                puts("i love it i= #{$i}")
+                $i+=1
+            end while $i<$num
